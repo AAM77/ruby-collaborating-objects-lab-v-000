c6db23140c864167b6442c61dd06ce588dcf8072
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-03-19 22:07:27 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+class Song
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end # initialize
+
+  def self.new_by_filename(filename)
+    song_name = filename.split(" - ")[1]
+    new_song = self.new(song_name)
+    new_song.artist_name = filename.split(" - ")[0]
+    new_song.save
+  end # .new_by_filename
+
+  def self.find_by_artist(artist)
+    Song.all.select do | song |
+      song.artist == artist
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    Artist.all << self.artist
+  end
+
+  def save
+    @@all << self
+    self
+  end
+end
