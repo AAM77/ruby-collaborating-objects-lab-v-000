4cd42ca7f32460ceab24ced909adb0213bb6319f
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-03-19 22:09:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
   def self.new_by_filename(filename)
     song_name = filename.split(" - ")[1]
     new_song = self.new(song_name)
-    new_song.artist_name= filename.split(" - ")[0]
+    new_song.artist_name = filename.split(" - ")[0]
     new_song.save
   end # .new_by_filename
 
